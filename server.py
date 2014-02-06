ae71534c0aac1e50cef611aaac108a9897a05028
rs2 <rootsumsquared@gmail.com>
2014-02-06 20:03:48 +0000

    Added get_mail_content

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
+import urllib2
 
 def get_mail_address(url):
-  body = get_mail_content()
-  return transform_mail_page(body)
+    body = get_mail_content()
+    return transform_mail_page(body)
+
+def get_mail_content(url):
+    """
+        Return HTML from a page
+    """
+    return urllib2.urlopen(url).read()
 
 def transform_mail_page(data):
-    return xxx(data)
+    pass
+#     return xxx(data)
 
 def serve(xxx):
     pass
+
+if __name__ == '__main__':
+    print get_mail_content('http://www.dailymail.co.uk/ushome/index.html')
