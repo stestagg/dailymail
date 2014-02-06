fcd302df6b322e6ee764c1d1b5d0b3d6ba1a21b6
Steve Stagg <stestagg@gmail.com>
2014-02-06 20:17:51 +0000

    Run a proxy server

--- server.py
+++ server.py
@@ -1,4 +1,26 @@
 import urllib2
+import SimpleHTTPServer
+import SocketServer
+import re
+import sys
+import urllib
+
+PORT = int(sys.argv[1])
+
+MAIL_RE = re.compile("http://.*\.dailymail\.co\.uk/")
+
+class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    
+    def do_GET(self):
+        print "GOT: %s" % (self.path, )
+        path = self.path
+        if MAIL_RE.match(path):
+            print self.rfile.read()
+            self.rfile.write(get_mail_address(path))
+        else:
+            self.copyfile(urllib.urlopen(self.path), self.wfile)
+            self.wfile.flush()
+httpd = SocketServer.TCPServer(("", PORT), Handler)
 
 def get_mail_address(url):
     """
@@ -21,6 +43,5 @@ def serve(xxx):
     pass
 
 if __name__ == '__main__':
-    print get_mail_address('http://www.dailymail.co.uk/ushome/index.html')[:100]
-    
-    print get_mail_address('http://i.dailymail.co.uk/i/sitelogos/logo_mol.gif')[:100]
+  print "Running proxy at: ", PORT
+  httpd.serve_forever()
